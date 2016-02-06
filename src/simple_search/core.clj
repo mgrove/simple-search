82fd9c19707392f89fb574637b1d289eafece00f
mgrove <grove266@morris.umn.edu>
2016-02-06 14:51:17 -0600

    finished toggle-1-item

--- src/simple_search/core.clj
+++ src/simple_search/core.clj
@@ -55,12 +55,29 @@
          (map add-score
               (repeatedly max-tries #(random-answer instance)))))
 
-;(defn toggle-1-item
-;  [answer index]
-;  (if (= 0 (nth (:choices answer) index))
-;    ()
-;    ())
+(defn toggle-1-item
+  [answer index]
+  (let [add-factor (if (= 0 (nth (:choices answer) index)) 1 -1)
+        new-list (if (= -1 add-factor)
+                   (apply list (assoc (into [] (:choices answer)) index 1))
+                   (apply list (assoc (into [] (:choices answer)) index 0)))
+        changed-item (nth (:items (:instance answer)) index)
+        new-total-weight (+ (:total-weight answer) (* add-factor (:weight changed-item)))
+        new-total-value (+ (:total-value answer) (* add-factor (:value changed-item)))
+        new-answer-without-score (assoc answer :choices new-list
+                                               :total-weight new-total-weight
+                                               :total-value new-total-value)]
+    (add-score new-answer-without-score)))
+
 
 
 (time (random-search knapPI_16_20_1000_1 100
 ))
+
+(toggle-1-item {:instance {:capacity 994, :items '({:value 403N, :weight 94N} {:value 886N, :weight 506N} {:value 814N, :weight 416N} {:value 1151N, :weight 992N} {:value 983N, :weight 649N} {:value 629N, :weight 237N} {:value 848N, :weight 457N} {:value 1074N, :weight 815N} {:value 839N, :weight 446N} {:value 819N, :weight 422N} {:value 1062N, :weight 791N} {:value 762N, :weight 359N} {:value 994N, :weight 667N} {:value 950N, :weight 598N} {:value 111N, :weight 7N} {:value 914N, :weight 544N} {:value 737N, :weight 334N} {:value 1049N, :weight 766N} {:value 1152N, :weight 994N} {:value 1110N, :weight 893N})}, :choices '(0 1 0 0 0 1 0 0 0 0 0 0 1 1 0 0 1 0 0 0), :total-weight 2436N, :total-value 4599N, :score -2436N} 0
+            )
+
+(apply list (assoc (into [] '(1 2 3)) 0 0))
+
+(assoc {:a 1 :b 2} :a 5 :b 7)
+
