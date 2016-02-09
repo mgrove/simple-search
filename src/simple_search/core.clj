23475280f2252b5a289fa93c6c7b66f8dce7139d
mgrove <grove266@morris.umn.edu>
2016-02-09 13:48:20 -0600

    Fixed bug in weight and value calculation in toggle-1-item.

--- src/simple_search/core.clj
+++ src/simple_search/core.clj
@@ -58,10 +58,10 @@
 (defn toggle-1-item
   "changes a value in :choices either from 0 to 1 or 1 to 0 to add or remove an item, then updates weight, value, and score"
   [answer index]
-  (let [add-factor (if (= 0 (nth (:choices answer) index)) 1 -1)
+  (let [add-factor (if (= 0 (nth (:choices answer) index)) 1 -1) ; -1 means we are removing from the knapsack
         new-list (if (= -1 add-factor)
-                   (apply list (assoc (into [] (:choices answer)) index 1))
-                   (apply list (assoc (into [] (:choices answer)) index 0)))
+                   (apply list (assoc (into [] (:choices answer)) index 0))
+                   (apply list (assoc (into [] (:choices answer)) index 1)))
         changed-item (nth (:items (:instance answer)) index)
         new-total-weight (+ (:total-weight answer) (* add-factor (:weight changed-item)))
         new-total-value (+ (:total-value answer) (* add-factor (:value changed-item)))
@@ -80,14 +80,26 @@
             better-answer (max-key :score current-best tweaked-answer)]
       (recur better-answer (dec loops))))))
 
+(defn random-restart
+  [tweak-fn number-of-iterations number-of-restarts instance]
+  (loop [current-best (hill-climb (add-score (random-answer instance)) tweak-fn number-of-iterations)
+         loops number-of-restarts]
+    (if (zero? loops)
+      current-best
+      (let [next-climb (hill-climb (add-score (random-answer instance)) tweak-fn number-of-iterations)
+            better-climb (max-key :score current-best next-climb)]
+      (recur better-climb (dec loops))))))
+
 ; Score is -2436
 (def test-answer {:instance {:capacity 994, :items '({:value 403N, :weight 94N} {:value 886N, :weight 506N} {:value 814N, :weight 416N} {:value 1151N, :weight 992N} {:value 983N, :weight 649N} {:value 629N, :weight 237N} {:value 848N, :weight 457N} {:value 1074N, :weight 815N} {:value 839N, :weight 446N} {:value 819N, :weight 422N} {:value 1062N, :weight 791N} {:value 762N, :weight 359N} {:value 994N, :weight 667N} {:value 950N, :weight 598N} {:value 111N, :weight 7N} {:value 914N, :weight 544N} {:value 737N, :weight 334N} {:value 1049N, :weight 766N} {:value 1152N, :weight 994N} {:value 1110N, :weight 893N})}, :choices '(0 1 0 0 0 1 0 0 0 0 0 0 1 1 0 0 1 0 0 0), :total-weight 2436N, :total-value 4599N, :score -2436N}
   )
 
+(random-restart toggle-1-item 100 1000 knapPI_16_20_1000_1)
+
 (hill-climb (add-score (random-answer knapPI_16_20_1000_1)) toggle-1-item 100000
             )
 
-(time (random-search knapPI_16_20_1000_1 100000
+(time (random-search knapPI_13_20_1000_1 100000
 ))
 
 (toggle-1-item {:instance {:capacity 994, :items '({:value 403N, :weight 94N} {:value 886N, :weight 506N} {:value 814N, :weight 416N} {:value 1151N, :weight 992N} {:value 983N, :weight 649N} {:value 629N, :weight 237N} {:value 848N, :weight 457N} {:value 1074N, :weight 815N} {:value 839N, :weight 446N} {:value 819N, :weight 422N} {:value 1062N, :weight 791N} {:value 762N, :weight 359N} {:value 994N, :weight 667N} {:value 950N, :weight 598N} {:value 111N, :weight 7N} {:value 914N, :weight 544N} {:value 737N, :weight 334N} {:value 1049N, :weight 766N} {:value 1152N, :weight 994N} {:value 1110N, :weight 893N})}, :choices '(0 1 0 0 0 1 0 0 0 0 0 0 1 1 0 0 1 0 0 0), :total-weight 2436N, :total-value 4599N, :score -2436N} 0
